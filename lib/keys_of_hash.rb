ff912da13ff27f46da1dcdb18a0395802f48a099
Robert Chen <robertchen234@gmail.com>
2018-10-22 19:13:36 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,29 +1,15 @@
-# class Hash
-  
-#   def keys_of(*arguments)
-#     arr = []
-#     arguments.each do |arg|
-#       arg.each do |key, val|
-#         if arg == val 
-#           arr << key 
-#         end 
-#       end 
-#     end
-#     arr
-#   end
-  
-# end
-
 class Hash
-  def keys_of(arguments)	  def keys_of(*argument)
-    # code goes here	listOfKeys=[]
- argument.each do |arg|
-    map do |key,value|
-     if arg == value
-   listOfKeys<< key
-      end
-     end
+  
+  def keys_of(*arguments)
+    arr = []
+    arguments.each do |arg|
+      arg.each do |key, val|
+        if arg == val 
+          arr << key 
+        end 
+      end 
     end
-listOfKeys
-  end	  end
-end 
+    arr
+  end
+  
+end
