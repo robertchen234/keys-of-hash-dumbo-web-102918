bf550c0c829676500fe3b3b3842a62f1d829950b
Robert Chen <robertchen234@gmail.com>
2018-10-22 19:21:31 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -11,17 +11,3 @@ class Hash
   end
   
 end
-
-# class Hash
-  
-#   def keys_of(*arguments)
-#   array = []
-#     self.each do |k, v|
-#       if arguments.include?(v)
-#         array.push(k)
-#       end
-#     end
-#   return array
-#   end	 
-  
-# end
