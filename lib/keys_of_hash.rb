400c8459d2843e7dd7c77911661e8ec0276b36e6
Robert Chen <robertchen234@gmail.com>
2018-10-22 19:14:26 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
   def keys_of(*arguments)
     arr = []
     arguments.each do |arg|
-      arg.map do |key, val|
+      arg.each do |key, val|
         if arg == val 
           arr << key 
         end 
